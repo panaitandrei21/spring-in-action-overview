24e6777680e69a7ef76b65377e2d53c6b9012eba
panaitandrei21 <panait.stefan2112@gmail.com>
2024-11-20 08:25:00 +0200

    spring data jdbc + load in datavase through ApplicationRunner or CommandLineRunner

---
+++ taco-cloud/src/main/java/tacos/Ingredient.java
@@ -0,0 +1,30 @@
+package tacos;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@Table
+@AllArgsConstructor
+@NoArgsConstructor(access= AccessLevel.PRIVATE, force=true)
+public class Ingredient implements Persistable<String> {
+
+    @Id
+    private String id;
+    private final String name;
+    private final Type type;
+
+    @Override
+    public boolean isNew() {
+        return false;
+    }
+
+    public enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
---
+++ taco-cloud/src/main/java/tacos/data/IngredientRepository.java
@@ -0,0 +1,9 @@
+package tacos.data;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import tacos.Ingredient;
+public interface IngredientRepository
+        extends CrudRepository<Ingredient, String> {
+
+}
